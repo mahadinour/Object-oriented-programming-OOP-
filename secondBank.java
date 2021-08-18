48fca3f258e85ac4de697c7b57492865823da267
mahadinour <88940854+mahadinour@users.noreply.github.com>
2021-08-18 09:26:35 +0600

    Add files via upload

---
+++ secondBank.java
@@ -0,0 +1,9 @@
+package bank;
+
+public class secondBank {
+    public static void main(String[] args) {
+        acc3 obj = new acc3("Tim Statler", "A00001");
+        acc3 obj1 = new acc3("Beyonce", "B00002");
+        obj.showMenu();
+    }
+}
